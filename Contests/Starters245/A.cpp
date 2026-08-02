751d0319f9ea7666234722877ce1c7d019ad6fd3
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-03 04:43:04 +0600

    Add A.cpp for contest problem solution

---
+++ Contests/Starters245/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    
+    if (S[0] == 'c' || S[3] == 'f') {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    
+    return 0;
+}
