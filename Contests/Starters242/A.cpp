d7cd3e256f96fa4fb7792b9826662171058e0a8f
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-05 22:07:40 +0600

    Add A.cpp to check divisibility by 5

---
+++ Contests/Starters242/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    if (cin >> x) {
+        if (x % 5 == 0) {
+            cout << "No" << endl;
+        } else {
+            cout << "Yes" << endl;
+        }
+    }
+    return 0;
+}
