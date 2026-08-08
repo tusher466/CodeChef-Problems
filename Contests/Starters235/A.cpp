38459abe150df268b910cf13d654972dc3a6841b
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-08 21:59:02 +0600

    Add comparison program in A.cpp
    Implement a simple comparison program that checks if y is less than or equal to x and outputs 0 or 100 accordingly.

---
+++ Contests/Starters235/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int x, y;
+    cin >> x >> y;
+
+    if (y <= x) {
+        cout << 0 << endl;
+    } else {
+        cout << 100 << endl;
+    }
+
+    return 0;
+}
