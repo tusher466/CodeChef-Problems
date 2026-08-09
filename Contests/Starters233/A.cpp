277bf26d83d339c757bcbe085a617e9ff01dc626
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-09 23:46:03 +0600

    Add A.cpp for contest problem solution

---
+++ Contests/Starters233/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n < 5)
+    {
+        cout << 100 * n << endl;
+    }
+    else
+    {
+        cout << 85 * n << endl;
+    }
+    
+    return 0;
+}
