3879149b77828bf1918ce50c35ab5579b53fdb62
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-10 20:16:36 +0600

    Add A.cpp for contest starter solution

---
+++ Contests/Starters232/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if(n < 7)
+    {
+        cout << n << endl;
+    }
+    else
+    {
+        cout << 7 << endl;
+    }
+
+    return 0;
+}
