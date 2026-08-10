d70eb1803f5bb76413b36deabd0039d25d788133
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-10 20:18:00 +0600

    Add A.cpp to check for character 's' in input

---
+++ Contests/Starters231/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string A;
+    cin >> A;
+    
+    if (A[0] == 's' || A[3] == 's') {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+    
+    return 0;
+}
