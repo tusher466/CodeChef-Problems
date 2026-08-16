f1b7bb3e923e4743be6dbe71fb9e64d4b0403771
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-16 23:09:00 +0600

    Add A.cpp for contest problem solution

---
+++ Contests/Starters222/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int x;
+    cin >> x;
+    
+    int remaining = x - 100;
+    int ans = remaining / 50;
+    
+    cout << ans << endl;
+    
+    return 0;
+}
