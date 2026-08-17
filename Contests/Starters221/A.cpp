aeef215f221c7a25e5b1d701ecb5226ac42bad22
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-17 23:46:07 +0600

    Add A.cpp for contest problem solution

---
+++ Contests/Starters221/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int x, y;
+    if (cin >> x >> y) {
+        if (x >= y) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
