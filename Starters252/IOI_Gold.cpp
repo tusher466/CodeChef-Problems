cb06ee65d56b6f8ea6e59bd1ef5a17e16696cca3
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-19 23:53:50 +0600

    Add IOI_Gold.cpp for input comparison

---
+++ Starters252/IOI_Gold.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, g;
+    cin >> n >> g;
+    
+    if (n >= g) {
+        cout << "Yes\n";
+    } else {
+        cout << "No\n";
+    }
+    
+    return 0;
+}
