50d22bbaaf07ad312dd4fdb592034868571b7f48
Md. Tusher Hossen <mdtusherhossen701@gmail.com>
2026-08-29 23:19:05 +0600

    Add Point_Calculation.cpp for scoring calculation

---
+++ Contests/Starters246/Point_Calculation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    
+    int total_points = (a * 3) + (b * 1) + (c * 0);
+    
+    cout << total_points << endl;
+
+    return 0;
+}
